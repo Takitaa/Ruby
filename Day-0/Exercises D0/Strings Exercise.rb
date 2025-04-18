e8289b1c393a43b310730ccb53375e66bcca8861
Takitaa <takitaplay@gmail.com>
2025-04-19 00:12:14 +0200

    Exercise D0 and VS Terminal color

---
+++ Day-0/Exercises D0/Strings Exercise.rb
@@ -0,0 +1,15 @@
+# in irb
+
+# 1. how long is the string "I really think I ma getting the hang of Ruby now"
+## 'I really think I am getting the hang of Ruby now.length'
+
+# 2. turn around the string " I live these exercises"
+## 'I like this exercises'.reverse
+
+# 3. Do the following String include the letter "m" : "Come on, let's learn Ruby!"
+
+
+
+# 4. The whole String in capital letters: "I am learning Ruby!"
+
+# 5.The whole String in small letters: "Let's code Ruby"
