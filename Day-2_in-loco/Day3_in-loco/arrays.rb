823ae33310aeab56f92726fd4a223e8fdb55381c
Takitaa <takitaplay@gmail.com>
2025-06-02 19:00:42 +0200

    Day3_in-loco

---
+++ Day-2_in-loco/Day3_in-loco/arrays.rb
@@ -0,0 +1,5 @@
+puts "Please enter 3 numbers:"
+
+number1 = get.chomps
+number2 = get.chomps
+number3 = get.chomps
--- Day-2_in-loco/variables_3.rb
+++ Day-2_in-loco/variables_3.rb
@@ -3,7 +3,7 @@ celsius = gets.chomp
 
 puts "You entered " + celsius + " C, let me convert that for you..."
 
-fahrenheit = (celsius.to_f * 9/5)+32
+fahrenheit = (celsius.to_f * 9/5) + 32
 
 puts "Converting to Fahrenheit it's " + fahrenheit.to_s  + " F. Goodbye!!!! "
 
